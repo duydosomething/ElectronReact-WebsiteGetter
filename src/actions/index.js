696e8609b3ec1999cc67ea316a0477dd095ad00f
Duy Tong <duy.tong@nairv-024666mac.belkin.com>
2018-10-09 16:26:06 -0700

    added types for redux

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+import { ADD_SITE, GET_SITES, REMOVE_SITE } from './types';
+import electron from 'electron';
+
+const { ipcRenderer } = 'electron';
---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+export const ADD_SITE = 'add_site';
+export const GET_SITES = 'get_sites';
+export const DELETE_SITE = 'remove_site';
