f9c8c37fe28bce4f89422bbb55af51f4f30381f7
duydosomething <dugtony@gmail.com>
2018-10-09 23:15:23 -0700

    just added some constants for entry file

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,11 +1,15 @@
 const electron = require('electron');
 // Module to control application life.
-const app = electron.app;
+//
+const database = require('./database-promises');
+const { app, BrowserWindow, ipcMain } = electron;
+//const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+//const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
+
 require('electron-reload')(__dirname);
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -58,3 +62,5 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// ipcMain
