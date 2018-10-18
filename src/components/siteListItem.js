d8bb88ca213e7040aeaeda9636e293dd6bc235b6
Duy Tong <dtong92@gmail.com>
2018-10-18 16:28:02 -0700

    just some light work with listitem

--- src/components/siteListItem.js
+++ src/components/siteListItem.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const ListItem = (props) => {
-  return(
-    <ListItem key={props.id} button>
-      <ListItemIcon>
-        <FolderIcon />
-      </ListItemIcon>
+class siteListItem extends Component {
+  state = {
+    checked: false
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.targed.checked});
+  }
+  render() {
+    return (
+    <ListItem key={this.props.id} button>
+      <Checkbox
+        checked={this.state.checked}
+        onChange = {this.handleChange('checked')}
+        value = 'checked'
+      />
       <ListItemText
-        primary={props.value}
+        primary={this.props.value}
       />
     </ListItem>
-  )
+  )}
 
 }
+
+export default siteListItem;
